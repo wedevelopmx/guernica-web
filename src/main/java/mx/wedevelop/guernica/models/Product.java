c28ac880648a0327ebc38592e79bf08ecfd6e4ec
Cristian Colorado <ccoloradoc@gmail.com>
2017-02-27 16:26:20 -0600

    Style: Adding bootstrap styling

---
+++ src/main/java/mx/wedevelop/guernica/models/Product.java
@@ -0,0 +1,7 @@
+package mx.wedevelop.guernica.models;
+
+/**
+ * Created by colorado on 27/02/17.
+ */
+public class Product {
+}
---
+++ src/main/java/mx/wedevelop/guernica/services/ProductService.java
@@ -0,0 +1,7 @@
+package mx.wedevelop.guernica.services;
+
+/**
+ * Created by colorado on 27/02/17.
+ */
+public class ProductService {
+}
